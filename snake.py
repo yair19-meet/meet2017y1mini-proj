0aba5103d569939e691d11f5ad12fab11e49e406
yair19-meet <yair19@meet.mit.edu>
2017-08-01 16:57:07 +0300

    Stopping here

--- snake.py
+++ snake.py
@@ -53,28 +53,29 @@ RIGHT = 3
 
 direction = UP
 
+UP_EDGE = 250
+DOWN_EDGE = -250
+RIGHT_EDGE = 400
+LEFT_EDGE = -400
+
 def up():
     global direction
     direction = UP
-    move_snake()
     print("You pressed the up key!")
 
 def down():
     global direction
     direction = DOWN
-    move_snake()
     print("You pressed the down key!")
 
 def left():
     global direction
     direction = LEFT
-    move_snake()
     print("You pressed the left key!")
 
 def right():
     global direction
     direction = RIGHT
-    move_snake()
     print("You pressed the right key!")
 
 turtle.onkeypress(up, UP_ARROW)
@@ -89,7 +90,25 @@ def move_snake():
     x_pos = my_pos[0]
     y_pos = my_pos[1]
 
-    if direction == RIGHT:
+    new_pos = snake.pos()
+    new_x_pos = new_pos[0]
+    new_y_pos = new_pos[1]
+
+    if new_x_pos >= RIGHT_EDGE:
+        print("You hit the right edge! Game over!")
+        quit()
+    elif new_x_pos <= LEFT_EDGE:
+        print("You hit the left edge! Game over!")
+        quit()
+    elif new_y_pos >= UP_EDGE:
+        print("You hit the up edge! Game over!")
+        quit()
+    elif new_y_pos <= DOWN_EDGE:
+        print("You hit the down edge! Game over!")
+        quit() 
+
+
+    elif direction == RIGHT:
         snake.goto(x_pos + SQUARE_SIZE, y_pos)
         print("You moved right!")
     elif direction == LEFT:
@@ -101,6 +120,10 @@ def move_snake():
     elif direction == DOWN:
         snake.goto(x_pos, y_pos - SQUARE_SIZE)
 
+    turtle.ontimer(move_snake, TIME_STEP)
+
+    
+
     my_pos = snake.pos()
     pos_list.append(my_pos)
     new_stamp = snake.stamp()
@@ -109,6 +132,28 @@ def move_snake():
     old_stamp = stamp_list.pop(0)
     snake.clearstamp(old_stamp)
     pos_list.pop(0)
+
+    if snake.pos() in food_pos:
+        food_ind = food_pos.
+
+
+    
+
+
+turtle.register_shape("trash.gif")
+
+food = turtle.clone()
+food.shape("trash.gif")
+
+food_pos = [(100, 100), (-100, 100), (-100, -100), (100, -100)]
+food_stamps = []
+
+food.hideturtle()
+
+for this_food_pos in food_pos:
+    food.goto(this_food_pos)
+    f_stamp = food.stamp()
+    food_stamps.append(f_stamp)
     
     
 
